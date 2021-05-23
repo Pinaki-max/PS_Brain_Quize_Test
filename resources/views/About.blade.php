53db6ecb65c1fe0a027663e175d9616f43b26d05
Pinaki-max <60256347+Pinaki-max@users.noreply.github.com>
2021-05-23 20:20:31 +0530

    ctreate layouts

---
+++ resources/views/About.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.master')
+@section('title','About')
+@section('content')
+    
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <h3>About Page</h3>
+        </div>
+    </div>
+</div>
+    
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.master')
+@section('title','Home')
+@section('content')
+    
+<div class="container">
+    <div class="row">
+        <div class="col-md-10">
+            <h3>Home Page</h3>
+        </div>
+    </div>
+</div>  
+@endsection
