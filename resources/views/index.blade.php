c95bf0df37278212ae75546c045c6b39e8f51e2b
Pinaki-max <60256347+Pinaki-max@users.noreply.github.com>
2021-06-02 20:12:00 +0530

    color change

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,19 +46,19 @@
   </div>
 
 
-  <div class="column" style="background-color:#aaa;">
+  <div class="column" style="background-color:rgb(38, 8, 204);">
     <h2>Column 1</h2>
     <p>Some text..</p>
   </div>
-  <div class="column" style="background-color:#bbb;">
+  <div class="column" style="background-color:rgb(38, 8, 204);">
     <h2>Column 2</h2>
     <p>Some text..</p>
   </div>
-  <div class="column" style="background-color:#ccc;">
+  <div class="column" style="background-color:rgb(38, 8, 204);">
     <h2>Column 3</h2>
     <p>Some text..</p>
   </div>
-  <div class="column" style="background-color:#ddd;">
+  <div class="column" style="background-color:rgb(38, 8, 204);">
     <h2>Column 4</h2>
     <p>Some text..</p>
   </div>
